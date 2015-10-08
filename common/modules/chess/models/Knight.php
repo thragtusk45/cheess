6abdb55b585690f911a3986f5cc6fa6b97726464
root <root@alex.org>
2015-10-08 14:51:02 +0300

    =all pieces

---
+++ common/modules/chess/models/Knight.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 08.10.15
+ * Time: 14:24
+ */
+
+namespace common\modules\chess\models;
+
+
+class Knight
+{
+
+}
---
+++ common/modules/chess/models/Queen.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 08.10.15
+ * Time: 14:25
+ */
+
+namespace common\modules\chess\models;
+
+
+class Queen
+{
+
+}
